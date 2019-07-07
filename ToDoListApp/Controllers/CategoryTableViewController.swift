0a7b865687071d4387378564db38aeb80ea71709
Jesse Miara <jessemiara01@gmail.com>
2019-07-06 19:01:48 -0500

    Categories are saving to Realm

--- ToDoListApp/Controllers/CategoryTableViewController.swift
+++ ToDoListApp/Controllers/CategoryTableViewController.swift
@@ -21,25 +21,63 @@ class CategoryTableViewController: UITableViewController {
         print(realm.configuration.fileURL)
 
     }
+    // MARK: - Data handling methods
+    
+    func saveToList(category : Categories){
+        do{
+            try realm.write {
+                realm.add(category)
+                print("\(category.categoryName) Written to database")
+            }
+        } catch{
+            print("Could not write to database \(error)")
+        }
+    }
 
     // MARK: - Table view data source
 
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return categoryList?.count ?? 0
+        return categoryList?.count ?? 1
     }
 
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
         
         cell.textLabel?.text = categoryList?[indexPath.row].categoryName ?? "No data entered"
         
-        
-
         return cell
     }
+    
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "Add a new category", message: "", preferredStyle: .alert)
+        var textField = UITextField()
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+        
+            let category = Categories()
+            
+            category.categoryName = textField.text!
+            
+            self.saveToList(category: category)
+            self.tableView.reloadData()
+            
+            
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Category"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
 
 
 
